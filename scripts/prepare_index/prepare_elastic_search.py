c64d778eab0d7124405b07aa4401de802e617a09
zhangzhuocheng <zhuocheng_zhang@163.com>
2024-06-27 16:31:03 +0800

    optimize script for prepare bm25 index

--- scripts/prepare_index/prepare_elastic_search.py
+++ scripts/prepare_index/prepare_elastic_search.py
@@ -1,5 +1,4 @@
 import json
-import os
 import pathlib
 import sys
 from argparse import ArgumentParser
@@ -12,29 +11,30 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
 from kylin.retriever import BM25Retriever, LocalRetriever, Retriever
 
 
-def read_data(file_path: str) -> Iterable:
-    if file_path.endswith(".jsonl"):
-        with open(file_path, "r") as f:
-            for line in f:
-                yield json.loads(line)
-    elif file_path.endswith(".tsv"):
-        title = []
-        with open(file_path, "r") as f:
-            for i, row in enumerate(reader(f, delimiter="\t")):
-                if i == 0:
-                    title = row
-                else:
-                    yield dict(zip(title, row))
-    elif file_path.endswith(".csv"):
-        title = []
-        with open(file_path, "r") as f:
-            for i, row in enumerate(reader(f)):
-                if i == 0:
-                    title = row
-                else:
-                    yield dict(zip(title, row))
-    else:
-        raise ValueError(f"Unsupported file format: {file_path}")
+def read_data(file_paths: list[str]) -> Iterable:
+    for file_path in file_paths:
+        if file_path.endswith(".jsonl"):
+            with open(file_path, "r") as f:
+                for line in f:
+                    yield json.loads(line)
+        elif file_path.endswith(".tsv"):
+            title = []
+            with open(file_path, "r") as f:
+                for i, row in enumerate(reader(f, delimiter="\t")):
+                    if i == 0:
+                        title = row
+                    else:
+                        yield dict(zip(title, row))
+        elif file_path.endswith(".csv"):
+            title = []
+            with open(file_path, "r") as f:
+                for i, row in enumerate(reader(f)):
+                    if i == 0:
+                        title = row
+                    else:
+                        yield dict(zip(title, row))
+        else:
+            raise ValueError(f"Unsupported file format: {file_path}")
 
 
 if __name__ == "__main__":
@@ -50,6 +50,7 @@ if __name__ == "__main__":
         "--corpus_path",
         type=str,
         required=True,
+        nargs="+",
         help="The path to the corpus file",
     )
     parser = Retriever.add_args(parser)
