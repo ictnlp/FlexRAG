a9770f14de2b3b9d7bc3e86b86aedf09b8e11527
zhangzhuocheng <zhuocheng_zhang@163.com>
2024-08-03 10:33:16 +0800

    optimize dense rewrite

--- kylin/searchers/dense_searcher.py
+++ kylin/searchers/dense_searcher.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, field
 
 from kylin.prompt import ChatTurn, ChatPrompt
 from kylin.retriever import DenseRetriever, DenseRetrieverConfig, RetrievedContext
+from kylin.utils import Choices
 
 from .searcher import BaseSearcher, BaseSearcherConfig, Searchers
 
@@ -15,7 +16,8 @@ logger = logging.getLogger("WebSearcher")
 @dataclass
 class DenseSearcherConfig(BaseSearcherConfig):
     retriever_config: DenseRetrieverConfig = field(default_factory=DenseRetrieverConfig)
-    rewrite_query: bool = False
+    rewrite_query: Choices(["never", "pseudo", "adaptive"]) = "never"  # type: ignore
+    max_rewrite_depth: int = 3
     retriever_top_k: int = 10
     disable_cache: bool = False
 
@@ -26,18 +28,33 @@ class DenseSearcher(BaseSearcher):
         super().__init__(cfg)
         # setup Dense Searcher
         self.rewrite = cfg.rewrite_query
+        self.rewrite_depth = cfg.max_rewrite_depth
         self.retriever_top_k = cfg.retriever_top_k
         self.disable_cache = cfg.disable_cache
 
         # load Dense Retrieve
         self.retriever = DenseRetriever(cfg.retriever_config)
 
-        # load prompt
-        self.rewrite_prompt = ChatPrompt.from_json(
+        # load prompts
+        self.rewrite_with_ctx_prompt = ChatPrompt.from_json(
             os.path.join(
                 os.path.dirname(__file__),
                 "searcher_prompts",
-                "dense_rewrite_prompt.json",
+                "rewrite_by_answer_with_context_prompt.json",
+            )
+        )
+        self.rewrite_wo_ctx_prompt = ChatPrompt.from_json(
+            os.path.join(
+                os.path.dirname(__file__),
+                "searcher_prompts",
+                "rewrite_by_answer_without_context_prompt.json",
+            )
+        )
+        self.verify_prompt = ChatPrompt.from_json(
+            os.path.join(
+                os.path.dirname(__file__),
+                "searcher_prompts",
+                "verify_prompt.json",
             )
         )
         return
@@ -45,24 +62,77 @@ class DenseSearcher(BaseSearcher):
     def search(
         self, question: str
     ) -> tuple[list[RetrievedContext], list[dict[str, object]]]:
-        if self.rewrite:
+        # rewrite the query
+        if self.rewrite == "pseudo":
             query_to_search = self.rewrite_query(question)
         else:
             query_to_search = question
-        ctxs = self.retriever.search(
-            query=[query_to_search],
-            top_k=self.retriever_top_k,
-            disable_cache=self.disable_cache,
-        )[0]
-        return ctxs, []
 
-    def rewrite_query(self, info: str) -> str:
+        # begin adaptive search
+        ctxs = []
+        search_history = []
+        verification = False
+        rewrite_depth = 0
+        while (not verification) and (rewrite_depth < self.rewrite_depth):
+            rewrite_depth += 1
+
+            # search
+            ctxs = self.retriever.search(
+                query=[query_to_search],
+                top_k=self.retriever_top_k,
+                disable_cache=self.disable_cache,
+            )[0]
+            search_history.append(
+                {
+                    "query": query_to_search,
+                    "ctxs": ctxs,
+                }
+            )
+
+            # verify the contexts
+            if self.rewrite == "adaptive":
+                verification = self.verify_contexts(ctxs, question)
+            else:
+                verification = True
+
+            # adaptive rewrite
+            if (not verification) and (rewrite_depth < self.rewrite_depth):
+                if rewrite_depth == 1:
+                    query_to_search = self.rewrite_query(question)
+                else:
+                    query_to_search = self.rewrite_query(question, ctxs)
+        return ctxs, search_history
+
+    def rewrite_query(
+        self, question: str, contexts: list[RetrievedContext] = []
+    ) -> str:
         # Rewrite the query to be more informative
-        user_prompt = f"Query: {info}"
-        prompt = deepcopy(self.rewrite_prompt)
+        if len(contexts) == 0:
+            prompt = deepcopy(self.rewrite_wo_ctx_prompt)
+            user_prompt = f"Question: {question}"
+        else:
+            prompt = deepcopy(self.rewrite_with_ctx_prompt)
+            user_prompt = ""
+            for n, ctx in enumerate(contexts):
+                user_prompt += f"Context {n}: {ctx.text}\n\n"
+            user_prompt += f"Question: {question}"
         prompt.update(ChatTurn(role="user", content=user_prompt))
         query = self.agent.chat([prompt], generation_config=self.gen_cfg)[0][0]
         return query
+
+    def verify_contexts(
+        self,
+        contexts: list[RetrievedContext],
+        question: str,
+    ) -> bool:
+        prompt = deepcopy(self.verify_prompt)
+        user_prompt = ""
+        for n, ctx in enumerate(contexts):
+            user_prompt += f"Context {n}: {ctx.text}\n\n"
+        user_prompt += f"Question: {question}"
+        prompt.update(ChatTurn(role="user", content=user_prompt))
+        response = self.agent.chat([prompt], generation_config=self.gen_cfg)[0][0]
+        return "yes" in response.lower()
 
     def close(self) -> None:
         self.retriever.close()
